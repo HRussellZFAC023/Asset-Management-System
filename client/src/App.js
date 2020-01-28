ab6ab44dc66b174ddb72d701ffa5f3b9aa9f8f70
Aidan Smithers <porrige51122>
2020-01-28 16:29:13 +0000

    Fixed Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { withRouter, Switch, Route } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
-import Users from "./components/dashboard/Admin/Users";
+import Users from "./components/Dashboard/Admin/Users";
 import SignIn from "./components/SignIn/SignIn";
 import SignUp from "./components/SignUp/SignUp";
 import Home from "./components/Home/HomePage";
-import Dashboard from "./components/dashboard/Dashboard";
+import Dashboard from "./components/Dashboard/Dashboard";
 import NotFound from "./components/NotFound/NotFound";
 import "./App.scss";
 
